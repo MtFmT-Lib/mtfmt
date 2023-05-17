49c815ecfa11fac78c9c9dc03759f9784886a5b7
向阳 <hinata.hoshino@qq.com>
2023-05-17 17:07:27 +0800

    add: svelte

--- www/src/index.ts
+++
@@ -1,3 +0,0 @@
-// SPDX-License-Identifier: LGPL-3.0
-'哼哼哼 啊啊啊啊啊啊啊啊啊啊啊啊'
-
---
+++ www/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<!-- 
+#  SPDX-License-Identifier: LGPL-3.0
+-->
+
+<svelte:head>
+    <title>Mtfmt</title>
+    <meta name="description" content="MtFmt library" />
+</svelte:head>
+<section>
+    <h1>114514</h1>
+</section>
