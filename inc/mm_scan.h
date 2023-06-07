e18436136e7b9b7162f8de2b50400e26375ebc11
向阳 <hinata.hoshino@qq.com>
2023-06-07 23:38:15 +0800

    chg: 独立开parser

---
+++ inc/mm_scan.h
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: LGPL-3.0
+/**
+ * @file    mm_scan.h
+ * @author  向阳 (hinata.hoshino@foxmail.com)
+ * @brief   格式化字串输入
+ * @version 1.0
+ * @date    2023-06-07
+ *
+ * @copyright Copyright (c) 向阳, all rights reserved.
+ *
+ */
+#if !defined(_INCLUDE_MM_SCAN_H_)
+#define _INCLUDE_MM_SCAN_H_ 1
+#include "mm_cfg.h"
+#include "mm_fmt.h"
+#include "mm_string.h"
+#include "mm_typedef.h"
+
+#endif // _INCLUDE_MM_SCAN_H_
