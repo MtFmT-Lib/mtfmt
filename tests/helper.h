3cc88bbc54e5be4b93844a18db8f226a2592706d
向阳 <hinata.hoshino@qq.com>
2023-06-15 18:48:45 +0800

    del: statement expr

--- tests/helper.h
+++ tests/helper.h
@@ -15,31 +15,30 @@
 #include "unity.h"
 
 /**
- * @brief 判断字符串str和字面值target_str是否一致
+ * @brief 确定2个字符串相等
  *
  */
-#define IS_EQUAL_STRING(str, target_str)       \
-    ({                                         \
+#define ASSERT_EQUAL_STRING(str, target_str)   \
+    do {                                       \
         MString target;                        \
         mstr_create(&target, (target_str));    \
         bool_t res = mstr_equal(str, &target); \
         mstr_free(&target);                    \
-        res;                                   \
-    })
-
-/**
- * @brief 确定2个字符串相等
- *
- */
-#define ASSERT_EQUAL_STRING(str, target_str) \
-    TEST_ASSERT_TRUE(IS_EQUAL_STRING(str, target_str))
+        TEST_ASSERT_TRUE(res);                 \
+    } while (0)
 
 /**
  * @brief 确定2个字符串不相等
  *
  */
 #define ASSERT_NOT_EQUAL_STRING(str, target_str) \
-    TEST_ASSERT_FALSE(IS_EQUAL_STRING(str, target_str))
+    do {                                         \
+        MString target;                          \
+        mstr_create(&target, (target_str));      \
+        bool_t res = mstr_equal(str, &target);   \
+        mstr_free(&target);                      \
+        TEST_ASSERT_FALSE(res);                  \
+    } while (0)
 
 /**
  * @brief 对某个表达式求值
